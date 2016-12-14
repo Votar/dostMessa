75fdc6d6d67e07de35c70770bacf27efb50a8935
Vyacheslav Lipchenko <nowlvs@gmail.com>
2016-12-14 17:48:07 +0200

    registration cleaned

--- app/src/main/java/entrego/com/entrego/ui/registration/model/EntregoRegistration.kt
+++ app/src/main/java/entrego/com/entrego/ui/registration/model/EntregoRegistration.kt
@@ -1,22 +1,14 @@
 package entrego.com.entrego.ui.auth.model
 
-import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonElement
-import entrego.com.entrego.util.Logger
+import entrego.com.entrego.storage.model.EntregoPhoneModel
 import entrego.com.entrego.web.api.ApiCreator
 import entrego.com.entrego.web.api.EntregoApi
-import entrego.com.entrego.web.model.request.auth.AuthBody
-import entrego.com.entrego.storage.model.EntregoPhoneModel
-import entrego.com.entrego.web.model.response.EntregoResult
 import entrego.com.entrego.web.model.request.registration.RegistrationBody
 import entrego.com.entrego.web.model.response.common.FieldErrorResponse
 import entrego.com.entrego.web.model.response.registration.EntregoResultRegistration
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.io.IOException
-import java.util.*
 
 /**
  * Created by bertalt on 29.11.16.
@@ -44,16 +36,10 @@ class EntregoRegistration(val email: String,
                             val result = response?.body()!!
                             when (result.code) {
                                 0 -> listener.onSuccessRegistration()
-                                1 -> {
-                                    if (result.fields.isNotEmpty()) {
+                                1 -> if (result.fields.isNotEmpty()) {
                                         for (next in result.fields)
                                             listener.onValidationError(next)
                                     }
-                                }
-                                2 -> {
-
-                                    listener.onFailureRegistration(result.message)
-                                }
                                 else -> listener.onFailureRegistration("")
 
                             }
@@ -62,7 +48,7 @@ class EntregoRegistration(val email: String,
                     }
 
                     override fun onFailure(call: Call<EntregoResultRegistration>?, t: Throwable?) {
-                        listener.onFailureRegistration("FUCK" + t?.message)
+                        listener.onFailureRegistration("" + t?.message)
                     }
 
                 })
